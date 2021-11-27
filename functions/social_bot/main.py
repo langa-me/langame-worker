6cdf937e62cbc29c28cfe4574056da5ff87591ea
louis030195 <louis.beaumont@gmail.com>
2021-11-27 13:50:37 +0100

    fine tune GPT3 x2, complete discord and slack bot impl

--- functions/social_bot/main.py
+++ functions/social_bot/main.py
@@ -5,6 +5,7 @@ from api.api_pb2_grpc import ConversationStarterServiceStub
 import json
 import requests
 import logging
+from google.cloud import firestore
 
 # For more channel options, please see https://grpc.io/grpc/core/group__grpc__arg__keys.html
 CHANNEL_OPTIONS = [
@@ -13,38 +14,33 @@ CHANNEL_OPTIONS = [
     ("grpc.keepalive_timeout_ms", 10000),
 ]
 
-APPLICATION_ID = os.getenv("APPLICATION_ID")
+DISCORD_APPLICATION_ID = os.getenv("DISCORD_APPLICATION_ID")
+
+client = firestore.Client()
 
 
-def social_bot(message, context):
-    """Background Cloud Function to be triggered by Pub/Sub.
+def social_bot(data, context):
+    """Triggered by a change to a Firestore document.
     Args:
-         event (dict):  The dictionary with data specific to this type of
-                        event. The `@type` field maps to
-                         `type.googleapis.com/google.pubsub.v1.PubsubMessage`.
-                        The `data` field maps to the PubsubMessage data
-                        in a base64-encoded string. The `attributes` field maps
-                        to the PubsubMessage attributes if any is present.
-         context (google.cloud.functions.Context): Metadata of triggering event
-                        including `event_id` which maps to the PubsubMessage
-                        messageId, `timestamp` which maps to the PubsubMessage
-                        publishTime, `event_type` which maps to
-                        `google.pubsub.topic.publish`, and `resource` which is
-                        a dictionary that describes the service API endpoint
-                        pubsub.googleapis.com, the triggering topic's name, and
-                        the triggering event type
-                        `type.googleapis.com/google.pubsub.v1.PubsubMessage`.
-    Returns:
-        None. The output is written to Cloud Logging.
+        data (dict): The event payload.
+        context (google.cloud.functions.Context): Metadata for the event.
     """
-    import base64
+    logger = logging.getLogger("social_bot")
     logging.basicConfig(level=logging.INFO)
 
-    if "data" in message:
-        data = base64.b64decode(message["data"])
-        topics = data.decode("utf-8")
-    if not data:
-        logging.error(f"No data in message: {message}")
+    path_parts = context.resource.split("/documents/")[1].split("/")
+    collection_path = path_parts[0]
+    document_path = "/".join(path_parts[1:])
+
+    affected_doc = client.collection(collection_path).document(document_path)
+
+    if "topics" in data["value"]["fields"]:
+        topics = [
+            e["stringValue"]
+            for e in data["value"]["fields"]["topics"]["arrayValue"]["values"]
+        ]
+    if not topics:
+        logger.error(f"No topics in message: {data}")
         return
     with grpc.secure_channel(
         "conversation-starter-garupdicsa-uc.a.run.app:443",
@@ -53,23 +49,35 @@ def social_bot(message, context):
         stub = ConversationStarterServiceStub(channel)
         cs_request = ConversationStarterRequest()
         cs_request.topics.extend(topics)
+        logger.info("Requesting conversation starter with topics: %s", topics)
         response = stub.GetConversationStarter(cs_request)
+        logger.info(f"Response from conversation starter: {response}")
 
-        if message.attributes["social_software"] == "slack":
+        if data["value"]["fields"]["social_software"]["stringValue"] == "slack":
+            logger.info("Sending message to slack")
             requests.post(
-                message.attributes["response_url"],
+                data["value"]["fields"]["response_url"]["stringValue"],
                 data=json.dumps(
-                    {"text": response.conversation_starter, "username": "Langame"}
+                    {
+                        "text": response.conversation_starter,
+                        "username": "Langame",
+                        "response_type": "in_channel",
+                    }
                 ),
             )
-        elif message.attributes["social_software"] == "discord":
+        elif data["value"]["fields"]["social_software"]["stringValue"] == "discord":
+            logger.info("Sending message to discord")
+            interaction_token = data["value"]["fields"]["interaction_token"][
+                "stringValue"
+            ]
             # https://discord.com/developers/docs/resources/webhook#execute-webhook
             requests.post(
-                f"https://discord.com/api/v8/webhooks/{APPLICATION_ID}/{message.attributes['interaction_token']}",
+                f"https://discord.com/api/v8/webhooks/{DISCORD_APPLICATION_ID}/{interaction_token}",
+                headers={"Content-Type": "application/json"},
                 data=json.dumps(
                     {
                         "content": response.conversation_starter,
                     }
                 ),
             )
-        message.ack()  # TODO: should actually check if everything went well
+        affected_doc.update({"conversation_starter": response.conversation_starter})
