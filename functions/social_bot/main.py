42198146aa4b0fad55b2f2e14deb086d71258d8d
louis030195 <louis.beaumont@gmail.com>
2021-12-03 11:53:05 +0100

    implement safety for social bots

--- functions/social_bot/main.py
+++ functions/social_bot/main.py
@@ -7,6 +7,7 @@ import requests
 import logging
 from google.cloud import firestore
 from random import choice
+from utils.messages import UNIMPLEMENTED_TOPICS_MESSAGES, FAILING_MESSAGES, PROFANITY_MESSAGES
 # For more channel options, please see https://grpc.io/grpc/core/group__grpc__arg__keys.html
 CHANNEL_OPTIONS = [
     ("grpc.lb_policy_name", "pick_first"),
@@ -17,29 +18,6 @@ CHANNEL_OPTIONS = [
 DISCORD_APPLICATION_ID = os.getenv("DISCORD_APPLICATION_ID")
 
 client = firestore.Client()
-
-# This is a list of funny messages that the bot will pick from to answer to the user
-# in case the API couldn't generate a conversation starter for the
-# given topics, suggesting known topics.
-UNIMPLEMENTED_TOPICS_MESSAGES = [
-    "I'm sorry, I haven't learned anything about that yet. ",
-    "I don't know what's on your mind right now. ",
-    "I'm sorry, I lack an opinion regarding that at the moment. ",
-    "I'm sorry, I don't know anything about that topic yet. ",
-    "I'm sorry, I'm not familiar with that topic. ",
-    "I'm sorry, I can't talk about that yet. ",
-    "I'm sorry, I should conceptualize this topic sometime soon. ",
-    "This is a tough topic, I don't know much about...",
-    "This feels very unfamiliar, I'm finding my way around this topic...",
-    "I'm sorry, I don't know anything about it... yet!",
-    "This topic is new to me, I'm not sure what to say. ",
-    "I haven't really learned much about this topic... ",
-    "I'm sorry, I should learn about this topic sooner. ",
-    "I can't think of anything interesting to say about this topic. ",
-    "How about we talk about something else? Try 'ice breaker,philosophy,travel,physic,ecology,artificial intelligence'",
-    "Please ask me something else, I have nothing to talk about this.",
-]
-# TODO: generate online messages
 
 def social_bot(data, context):
     """Triggered by a change to a Firestore document.
@@ -75,26 +53,47 @@ def social_bot(data, context):
         response = None
         error = None
         try:
+            # TODO?
+            # if data["value"]["fields"]["social_software"]["stringValue"] == "discord":
+            #     # POST/channels/{channel.id}/typing
+            #     logger.info(f"Sending typing indicator to discord")
+            #     channel_id = data["value"]["fields"]["channel_id"][
+            #         "stringValue"
+            #     ]
+            #     # https://discord.com/developers/docs/resources/webhook#execute-webhook
+            #     requests.post(
+            #         f"https://discord.com/api/v8/channels/{channel_id}/typing",
+            #     )
+
             response = stub.GetConversationStarter(cs_request)
             logger.info(f"Response from conversation starter: {response}")
-        except grpc.RpcError as e:
+        except Exception as e:
             logger.warn(f"Error from conversation starter: {e}")
             error = e
-
+        user_facing_message = ""
+        if response:
+            user_facing_message = response.conversation_starter
+        elif "profane" in str(error):
+            user_facing_message = choice(PROFANITY_MESSAGES)
+            user_facing_message = user_facing_message.replace("[TOPICS]", f"\"{','.join(topics)}\"")
+        elif "not-found" in str(error):
+            user_facing_message = choice(UNIMPLEMENTED_TOPICS_MESSAGES)
+        else:
+            user_facing_message = choice(FAILING_MESSAGES)
         if data["value"]["fields"]["social_software"]["stringValue"] == "slack":
-            logger.info("Sending message to slack")
+            logger.info(f"Sending message to slack {user_facing_message}")
             requests.post(
                 data["value"]["fields"]["response_url"]["stringValue"],
                 data=json.dumps(
                     {
-                        "text": response.conversation_starter if response else choice(UNIMPLEMENTED_TOPICS_MESSAGES),
+                        "text": user_facing_message,
                         "username": "Langame",
                         "response_type": "in_channel",
                     }
                 ),
             )
         elif data["value"]["fields"]["social_software"]["stringValue"] == "discord":
-            logger.info("Sending message to discord")
+            logger.info(f"Sending message to discord {user_facing_message}")
             interaction_token = data["value"]["fields"]["interaction_token"][
                 "stringValue"
             ]
@@ -104,7 +103,7 @@ def social_bot(data, context):
                 headers={"Content-Type": "application/json"},
                 data=json.dumps(
                     {
-                        "content": response.conversation_starter if response else choice(UNIMPLEMENTED_TOPICS_MESSAGES),
+                        "content": user_facing_message,
                     }
                 ),
             )
