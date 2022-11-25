c71ff6866309cf24faf718415417e7e3ac1a7ad1
Louis Beaumont <louis.beaumont@gmail.com>
2022-11-25 11:49:07 +0100

    fix

--- langame/functions/errors.py
+++ langame/functions/errors.py
@@ -1,6 +1,8 @@
 
 def init_errors(env: str = "development"):
     import sentry_sdk
+    from sentry_sdk.integrations.gcp import GcpIntegration
+
     sentry_sdk.init(
         "https://89b0a4a5cf3747ff9989710804f50dbb@o404046.ingest.sentry.io/6346831",
 
@@ -9,5 +11,7 @@ def init_errors(env: str = "development"):
         # We recommend adjusting this value in production.
         traces_sample_rate=1.0,
         environment=env,
+        integrations=[GcpIntegration()],
+
     )
 
