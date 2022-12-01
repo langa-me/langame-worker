11f8321b6cefb523234370f8b9befcfc41d7e8d3
Louis Beaumont <louis.beaumont@gmail.com>
2022-12-01 11:53:58 +0100

    add integration test + funny ci/cd

--- run/collection/test_all.py
+++ run/collection/test_all.py
@@ -6,18 +6,27 @@ import time
 # pylint: disable=C0116
 # pylint: disable=C0115
 import requests
+import random
+
+IS_LOCAL = os.environ.get("IS_TESTING", "false") == "true"
+URL = "http://127.0.0.1:8080" if IS_LOCAL else "https://api.langa.me"
+
+fun_topics = [
+    "fun",
+    "dating",
+    "big talk",
+    "sex",
+]
 
 # make run/collection/local
 class TestAll(IsolatedAsyncioTestCase):
     async def test_create_starter(self):
-
-        url = "http://127.0.0.1:8080/v1/conversation/starter"
+        url = f"{URL}/v1/conversation/starter"
         data = {
-            "personas": [
-                "I am a biology student, I like to play basketball on my free time",
-                "I am a computer science student, I like to play video games on my free time. I like topics such as philosophy",
-            ],
+            # pick 2 random topic
+            "topics": random.sample(fun_topics, 2),
         }
+        print("querying with topics: ", data["topics"])
         r = requests.post(
             url,
             headers={"X-Api-Key": os.environ["LANGAME_API_KEY"]},
@@ -25,10 +34,12 @@ class TestAll(IsolatedAsyncioTestCase):
             timeout=10000,
         )
         assert r.status_code == 200
+        data = r.json()
+        print("OUTPUT:", data["results"][0]["conversation_starter"]["en"])
 
     async def test_create_starter_scaled(self):
         start_time = time.time()
-        url = "http://127.0.0.1:8080/v1/conversation/starter"
+        url = f"{URL}/v1/conversation/starter"
         data = {
             "topics": [
                 "dating",
@@ -48,3 +59,10 @@ class TestAll(IsolatedAsyncioTestCase):
         end_time = time.time()
         print("Time taken: ", end_time - start_time)
         assert len(data["results"]) == 10
+
+
+# TODO test with personas
+# "personas": [
+#     "I am a biology student, I like to play basketball on my free time",
+#     "I am a computer science student, I like to play video games on my free time. I like topics such as philosophy",
+# ],
