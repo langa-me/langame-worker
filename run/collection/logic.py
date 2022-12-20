567c136b139042aa8bae5b722b78f55ccfd0c4aa
Louis Beaumont <louis.beaumont@gmail.com>
2022-12-20 14:52:35 +0100

    fix /conversation/collection/{collection_id}/starter 2d array result

--- run/collection/logic.py
+++ run/collection/logic.py
@@ -506,7 +506,7 @@ def get_collection_starter(
     return (
         jsonify(
             {
-                "results": [conversation_starters],
+                "results": conversation_starters,
             }
         ),
         200,
